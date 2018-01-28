717b227bd7aafe0986a22265bdbde9792c3a03ac
Michael B. Stoeckel <mstoeckel@cognodyne.com>
2018-01-27 19:25:04 -0500

    CdiUtil enhancements

--- src/main/java/com/cognodyne/dw/cdi/CdiUtil.java
+++ src/main/java/com/cognodyne/dw/cdi/CdiUtil.java
@@ -10,10 +10,15 @@ import java.util.regex.Pattern;
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
 
 import com.google.common.collect.Sets;
 
 public class CdiUtil {
+    public static BeanManager getBeanManager() {
+        return CDI.current().getBeanManager();
+    }
+
     public static boolean isAnnotationPresent(Bean<?> bean, Class<? extends Annotation> annotationClass) {
         return isAnnotationPresent(bean.getTypes(), annotationClass);
     }
@@ -79,14 +84,35 @@ public class CdiUtil {
     }
 
     @SuppressWarnings("unchecked")
+    public static <T> T getReference(Class<? extends T> cls, Annotation... qualifiers) {
+        Set<Bean<?>> beans = getBeanManager().getBeans(cls, qualifiers);
+        if (beans == null || beans.isEmpty()) {
+            return null;
+        }
+        return getReference((Bean<T>) beans.iterator().next());
+    }
+
+    public static <T> T getReference(Bean<T> bean) {
+        return getReference(getBeanManager(), bean);
+    }
+
+    @SuppressWarnings("unchecked")
     public static <T> T getReference(BeanManager bm, Bean<T> bean) {
         CreationalContext<T> ctx = bm.createCreationalContext(bean);
         return (T) bm.getReference(bean, bean.getBeanClass(), ctx);
     }
 
+    public static <T> T create(Bean<T> bean) {
+        return create(getBeanManager(), bean);
+    }
+
     public static <T> T create(BeanManager bm, Bean<T> bean) {
         CreationalContext<T> ctx = bm.createCreationalContext(bean);
         return bean.create(ctx);
+    }
+
+    public static <T> void destroy(Bean<T> bean) {
+        destroy(getBeanManager(), bean);
     }
 
     public static <T> void destroy(BeanManager bm, Bean<T> bean) {
